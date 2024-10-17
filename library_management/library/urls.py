4ae35460c084a4c393a3408b03b5200cd3ca5b1c
daniil Sapunov <daniilsapunov133@gmail.com>
2024-10-18 01:29:51 +0300

    commit pre-last

--- library_management/library/urls.py
+++ library_management/library/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from . import views
+from library import views
 
 urlpatterns = [
     path('authors/', views.author_list, name='author_list'),
     path('authors/create/', views.author_create, name='author_create'),
+    path('authors/update/<int:pk>/', views.author_update, name='author_update'),
+    path('authors/delete/<int:pk>/', views.author_delete, name='author_delete'),
+
     path('books/', views.book_list, name='book_list'),
     path('books/create/', views.book_create, name='book_create'),
+    path('books/update/<int:pk>/', views.book_update, name='book_update'),
+    path('books/delete/<int:pk>/', views.book_delete, name='book_delete'),
+
     path('readers/', views.reader_list, name='reader_list'),
     path('readers/create/', views.reader_create, name='reader_create'),
+    path('readers/update/<int:pk>/', views.reader_update, name='reader_update'),
+    path('readers/delete/<int:pk>/', views.reader_delete, name='reader_delete'),
 ]
--- library_management/library/views.py
+++ library_management/library/views.py
@@ -17,11 +17,34 @@ def author_create(request):
         form = AuthorForm()
     return render(request, 'library/author_form.html', {'form': form})
 
+def author_update(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, instance=author)
+        if form.is_valid():
+            form.save()
+            return redirect('author_list')
+    else:
+        form = AuthorForm(instance=author)
+    return render(request, 'library/author_form.html', {'form': form})
+
+def author_delete(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    if request.method == 'POST':
+        author.delete()
+        return redirect('author_list')
+    return render(request, 'library/author_confirm_delete.html', {'author': author})
+
 # Book CRUD
 def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'library/book_list.html', {'books': books})
+    authors = Author.objects.all()  # Получаем всех авторов для фильтрации
+    selected_author = request.GET.get('author')  # Получаем выбранного автора из параметров запроса
+    books = Book.objects.all()  # Получаем все книги
 
+    if selected_author:
+        books = books.filter(author_id=selected_author)  # Фильтруем книги по автору
+
+    return render(request, 'library/book_list.html', {'books': books, 'authors': authors, 'selected_author': selected_author})
 def book_create(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
@@ -31,6 +54,24 @@ def book_create(request):
     else:
         form = BookForm()
     return render(request, 'library/book_form.html', {'form': form})
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'library/book_form.html', {'form': form})
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'library/book_confirm_delete.html', {'book': book})
 
 # Reader CRUD
 def reader_list(request):
@@ -46,3 +87,20 @@ def reader_create(request):
     else:
         form = ReaderForm()
     return render(request, 'library/reader_form.html', {'form': form})
+
+def reader_update(request, pk):
+    reader = get_object_or_404(Reader, pk=pk)
+    if request.method == 'POST':
+        form = ReaderForm(request.POST, instance=reader)
+        if form.is_valid():
+            form.save()
+            return redirect('reader_list')
+    else:
+        form = ReaderForm(instance=reader)
+    return render(request, 'library/reader_form.html', {'form': form})
+def reader_delete(request, pk):
+    reader = get_object_or_404(Reader, pk=pk)
+    if request.method == 'POST':
+        reader.delete()
+        return redirect('reader_list')
+    return render(request, 'library/reader_confirm_delete.html', {'reader': reader})
